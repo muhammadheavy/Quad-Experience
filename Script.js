74c7522ce6eb7223111419d05606c8cb6dae66b9
muhammadheavy <rwqrwqxyz@gmail.com>
2025-03-29 13:57:39 -0300

    Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
   // ==UserScript==
   // @name         Quad Experience
-  // @version      1.0
+  // @version      1.2
   // @description  Quad Experience
   // @match        https://gota.io/web/*
   // @updateURL    https://raw.githubusercontent.com/SEU_USUARIO/quad-experience/main/quad-experience.user.js
